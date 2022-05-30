351db7ad5643994f1f796a515c37c086894c7c6b
gcosta2107 <garragr10@gmail.com>
2022-05-30 13:01:07 -0300

    add main menu

---
+++ src/MainMenu.gd
@@ -0,0 +1,50 @@
+extends MarginContainer
+
+const first_scene =  preload("res://Scenes/World.tscn")
+
+onready var selector_one = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer/HBoxContainer/Selector
+onready var selector_two = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer2/HBoxContainer/Selector
+onready var selector_three= $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer3/HBoxContainer/Selector
+onready var selector_four = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer4/HBoxContainer/Selector
+
+var current_selection = 0
+
+func _ready():
+	set_current_selection(0)
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_down") and current_selection < 3:
+		current_selection += 1
+		set_current_selection(current_selection)
+	elif Input.is_action_just_pressed("ui_up") and current_selection > 0:
+		current_selection -= 1
+		set_current_selection(current_selection)
+	elif Input.is_action_just_pressed("ui_accept"):
+		handle_selection(current_selection)
+
+func handle_selection(_current_selection):
+	if _current_selection == 0:
+		get_parent().add_child(first_scene.instance())
+		queue_free()
+	elif _current_selection == 1:
+		print("Adicionar saves")
+	elif _current_selection == 2:
+		print("Adicionar opcoes")
+	elif _current_selection == 3:
+		get_tree().quit()
+		
+
+func set_current_selection(_current_selection):
+	selector_one.text = ""
+	selector_two.text = ""
+	selector_three.text = ""
+	selector_four.text = ""
+	
+	if _current_selection == 0:
+		selector_one.text = ">"
+	elif _current_selection == 1:
+		selector_two.text = ">"
+	elif _current_selection == 2:
+		selector_three.text = ">"
+	elif _current_selection == 3:
+		selector_four.text = ">"
