2a7d55023f56786704484724fb661b9f3c71e9af
Gustavo Henrique <ghco@cesar.school>
2022-06-04 04:05:06 -0300

    New player & enemy sprites

--- src/BaseEnemy.gd
+++ src/BaseEnemy.gd
@@ -1,6 +1,6 @@
 extends Resource
 
-export(String) var name = "GABIRU"
+export(String) var name = "NAME ENEMY"
 export(Texture) var texture = null
 export(int) var health = 100
 export(int) var damage = 20
